8d30a4fae98db6db0e1099f1615be1ff5cef7d0b
sana <sana@gmail.com>
2019-12-31 17:21:58 +0500

    Initial Commit

---
+++ MerchantNation/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MerchantNation
+//
+//  Created by umer hamid on 12/31/19.
+//  Copyright © 2019 umer hamid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
